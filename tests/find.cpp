519257061b2ff1d063eecf0957f48074150c2f73
Loveskyrim <dr.konovalov@gmail.com>
2016-10-07 14:00:44 +0300

    Update find.cpp

--- tests/find.cpp
+++ tests/find.cpp
@@ -1,19 +1,14 @@
-
-//
-// Created by pca05 on 30.09.16.
-//
-
 #include "../include/BinarySearchTree.h"
 #include "catch.hpp"
 
-SCENARIO("if element is in tree, find method must return it's pointer") 
+SCENARIO("if element is in tree, find method must return it's pointer")
 {
-    GIVEN("tree") 
+    GIVEN("tree")
     {
         BinarySearchTree<int> tree{1, 4, 5};
-        WHEN("find") 
+        WHEN("find")
         {
-            THEN("return pointer to element must not be nullptr") 
+            THEN("return pointer to element must not be nullptr")
             {
                 REQUIRE(*(tree.find(4)) == 4);
             }
@@ -21,14 +16,14 @@ SCENARIO("if element is in tree, find method must return it's pointer")
     }
 }
 
-SCENARIO("if element is not in tree, find method must return nullptr") 
+SCENARIO("if element is not in tree, find method must return nullptr")
 {
-    GIVEN("tree") 
+    GIVEN("tree")
     {
         BinarySearchTree<int> tree{1, 4, 5};
-        WHEN("find") 
+        WHEN("find")
         {
-            THEN("return pointer to element must be nullptr") 
+            THEN("return pointer to element must be nullptr")
             {
                 REQUIRE(tree.find(2) == nullptr);
             }
@@ -36,18 +31,20 @@ SCENARIO("if element is not in tree, find method must return nullptr")
     }
 }
 
-SCENARIO("find method must work for constant tree and non-constant tree") 
+SCENARIO("find method must work for constant tree and non-constant tree")
 {
-    GIVEN("constant and non-constant tree") 
+    GIVEN("constant and non-constant tree")
     {
         const BinarySearchTree<int> tree1{1, -2, 0};
         BinarySearchTree<int> tree2{1, 4, 5};
-        WHEN("find") 
+        WHEN("find")
         {
-            THEN("return element for constant and non-constant tree") 
+            THEN("return element for constant and non-constant tree")
             {
-                REQUIRE(tree1.find(-2) && tree2.find(1));
+                REQUIRE(tree1.find(-2));
+                REQUIRE(tree2.find(1));
             }
         }
     }
 }
+
